b36fc4b334e8002be16dbcd4921618b861b85dde
Abraham <abrahamchalita@hotmail.com>
2022-03-09 01:20:51 -0600

    Added color functionalities, border and styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./styling.css";
 
 class App extends React.Component {
@@ -18,27 +18,35 @@ class App extends React.Component {
   };
 
   calculo = () => {
-    let estaturaFinal = ((this.state.altura)*(this.state.altura))/10000;
+    let estaturaFinal = ((this.state.altura)*(this.state.altura));
     let resultado_num = this.state.peso / estaturaFinal;
     let classificacion = "";
+    let alerta = "";
     
 
     if (resultado_num <= 18.5){
       classificacion = "Peso bajo";
-    }else if (resultado_num > 18.5 && resultado_num <= 24.9){
+      alerta = "danger";
+    }else if (resultado_num > 18.5 && resultado_num < 25){
       classificacion = "Peso saludable";
+      alerta = "success";
     }else if (resultado_num >= 25 && resultado_num <= 29.9) {
       classificacion = "Sobre Peso";
+      alerta = "warning";
     }else if (resultado_num >= 30 && resultado_num <= 34.9){
       classificacion = "Obesidad grado 1";
+      alerta = "danger";
     }else if (resultado_num >= 35 && resultado_num <= 39.9){
       classificacion = "Obesidad grado 2";
+      alerta = "danger";
     }else if (resultado_num >= 40){
       classificacion = "Obesidad morbida";
+      alerta = "danger";
     }
 
     this.setState({classificacion: classificacion});
-    this.setState({resultado_num: Math.round(resultado_num * 100) / 100 });
+    this.setState({alerta: alerta});
+    this.setState({resultado_num: Math.round(resultado_num * 100) / 100 }); 
   };
 
   handleSubmit = (event) => {
@@ -54,26 +62,46 @@ class App extends React.Component {
           <div>
             <label> Peso (kg) </label>
             <input type="number"
+                  step="0.1"
                   value={this.state.peso}
                   name="peso"
                   onChange={this.handleChange} />
           </div>
 
           <div>
-            <label> Altura (cm) </label>
+            <label> Altura (m) </label>
                 <input type="number"
+                      step="0.1"
                       value={this.state.altura}
                       name="altura"
                       onChange={this.handleChange} />
           </div>
         
             <button type="submit">Submit</button>
-            <h2> {this.state.resultado_num} </h2>
-            <h2> {this.state.classificacion} </h2>
+            <div>
+              <IMCdisplay res={this.state.resultado_num} label={this.state.classificacion} alertClass={this.state.alerta} />
+            </div>
           </form>
         </div>
       );
   }
+}
+
+function IMCdisplay(check){
+  let color
+  if(check.alertClass == "success"){
+    color = "#33D273"
+  }else if(check.alertClass == "warning"){
+    color = "#E8E81E"
+  }else if(check.alertClass == "danger"){
+    color = "#E84D1E"
+  }
+  return (
+    <div style={{backgroundColor: color}} className="res">
+      <div>{ check.res }</div>
+      <div>{ check.label }</div>
+    </div>  
+  )
 }
 
 export default App;
